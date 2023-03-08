b556290ffb210444a164cd689cc0a7935387f2c6
Andrey Pechennikov <ap@devandy.me>
2023-03-08 15:06:21 +0400

    Add config for users application

--- fit-friends/apps/users/src/app/app.module.ts
+++ fit-friends/apps/users/src/app/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoDbOptions, mongodbConfig } from '@fit-friends/core';
+
+import { ENV_FILE_PATH } from './app.constant';
+import { jwtConfig } from '../config/jwt.config';
+import { validateEnvironments } from './env.validation';
+
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: ENV_FILE_PATH,
+      load: [mongodbConfig, jwtConfig],
+      validate: validateEnvironments,
+    }),
+    MongooseModule.forRootAsync(
+      getMongoDbOptions()
+    ),
+  ]
 })
 export class AppModule {}
--- fit-friends/apps/users/src/main.ts
+++ fit-friends/apps/users/src/main.ts
@@ -3,19 +3,35 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DEFAULT_PORT, GLOBAL_PREFIX } from '@fit-friends/core';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+
+  app.setGlobalPrefix(GLOBAL_PREFIX);
+
+  const config = new DocumentBuilder()
+    .setTitle('The users service')
+    .setDescription('Users service API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true
+  }));
+
+  const port = process.env.PORT || DEFAULT_PORT;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}/${GLOBAL_PREFIX}`
   );
 }
 
